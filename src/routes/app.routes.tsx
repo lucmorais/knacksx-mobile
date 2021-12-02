83a370e79444f26e4fc50782def2b34ec71a866a
Luciano Morais <lucianopythonista@gmail.com>
2021-12-02 00:55:53 -0300

    inclusão de botão do login no header

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,13 +1,33 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../telas/Home";
+import { Button, View } from "react-native";
+import styles from "./styles";
+import { useAuth } from "../contexts/auth";
 
 const AppStack = createStackNavigator();
 
 export default function AppRoutes() {
+    const { logOut } = useAuth();
+
+    const handleLogOut = () => logOut();
+
     return (
         <AppStack.Navigator>
-            <AppStack.Screen name="Home" component={Home} />
+            <AppStack.Screen name="Home" component={Home} options={{
+                headerRight: () => (
+                    <View style={styles.botaoLogout}>
+                        <Button
+                            onPress={() => {
+                                handleLogOut();
+                                alert('Login feito com sucesso!');
+                            }}
+                            title="Logout"
+                            color="blue"
+                        />
+                    </View>
+                  ),
+            }} />
         </AppStack.Navigator>
     )
 }
--- src/telas/Home/index.tsx
+++ src/telas/Home/index.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Text } from 'react-native';
 import { useAuth } from '../../contexts/auth';
 import styles from './styles';
 
 export default function Home() {
-    const {logOut} = useAuth();
-
-    function handleLogOut() {
-        logOut();
-    }
+    const {user} = useAuth();
 
     return (
         <View style={styles.conteudo}>
-            <View> 
-                <Button title="Logout" onPress={handleLogOut} />
+            <View>
+                <Text>{user?.username}</Text>
             </View>
-        </View>
+        </View>   
     )
 }
